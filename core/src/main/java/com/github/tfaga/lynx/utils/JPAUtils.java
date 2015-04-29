372e68574b0d754d076b53897f6b5e1ae5dc613f
Tilen Faganel <tilen.faganel@me.com>
2015-04-29 15:25:43 +0200

    Added tests for jpa paging

--- core/src/main/java/com/github/tfaga/lynx/utils/JPAUtils.java
+++ core/src/main/java/com/github/tfaga/lynx/utils/JPAUtils.java
@@ -3,9 +3,9 @@ package com.github.tfaga.lynx.utils;
 import com.github.tfaga.lynx.beans.QueryParameters;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -15,8 +15,21 @@ import javax.persistence.criteria.Root;
  */
 public class JPAUtils {
 
+    private static final Logger log = Logger.getLogger(JPAUtils.class.getSimpleName());
+
+    public static <T> List<T> queryEntities(EntityManager em, Class<T> entity) {
+
+        return queryEntities(em, entity, new QueryParameters());
+    }
 
     public static <T> List<T> queryEntities(EntityManager em, Class<T> entity, QueryParameters q) {
+
+        if (q == null)
+            throw new IllegalArgumentException("Query parameters can't be null. " +
+                    "If you don't have any parameters either pass a empty object or " +
+                    "use the queryEntities(EntityManager, Class<T>) method.");
+
+        log.finest("Querying entity: '" + entity.getSimpleName() + "' with parameters: " + q);
 
         CriteriaQuery<T> cq = em.getCriteriaBuilder().createQuery(entity);
 
@@ -26,19 +39,17 @@ public class JPAUtils {
 
         TypedQuery<T> tq = em.createQuery(cq);
 
-        if (q.getLimit() != null) {
+        if (q.getLimit() != null && q.getLimit() > -1) {
 
             tq.setMaxResults(q.getLimit().intValue());
         }
 
-        if (q.getOffset() != null) {
+        if (q.getOffset() != null && q.getOffset() > -1) {
 
             tq.setFirstResult(q.getOffset().intValue());
         }
 
-        List<T> results = tq.getResultList();
-
-        return results;
+        return tq.getResultList();
     }
 
     public static <T> T getEntity(EntityManager em, Class<T> entity, Object id) {
--- core/src/test/java/com/github/tfaga/lynx/test/jpa/JPAUtilsQueryTest.java
+++ core/src/test/java/com/github/tfaga/lynx/test/jpa/JPAUtilsQueryTest.java
@@ -27,6 +27,97 @@ public class JPAUtilsQueryTest {
     public void testEmptyQuery() {
 
         List<User> users = JPAUtils.queryEntities(em, User.class, new QueryParameters());
+
+        Assert.assertNotNull(users);
+        Assert.assertEquals(users.size(), 100);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullQueryWrongMethod() {
+
+        JPAUtils.queryEntities(em, User.class, null);
+    }
+
+    @Test
+    public void testNullQuery() {
+
+        List<User> users = JPAUtils.queryEntities(em, User.class);
+
+        Assert.assertNotNull(users);
+        Assert.assertEquals(users.size(), 100);
+    }
+
+    @Test
+    public void testLimit() {
+
+        QueryParameters q = new QueryParameters();
+        q.setLimit(10);
+
+        List<User> users = JPAUtils.queryEntities(em, User.class, q);
+
+        Assert.assertNotNull(users);
+        Assert.assertEquals(users.size(), 10);
+        Assert.assertNotNull(users.get(0).getId());
+        Assert.assertEquals(users.get(0).getId().intValue(), 1);
+        Assert.assertNotNull(users.get(9).getId());
+        Assert.assertEquals(users.get(9).getId().intValue(), 10);
+    }
+
+    @Test
+    public void testOffset() {
+
+        QueryParameters q = new QueryParameters();
+        q.setOffset(30);
+
+        List<User> users = JPAUtils.queryEntities(em, User.class, q);
+
+        Assert.assertNotNull(users);
+        Assert.assertEquals(users.size(), 70);
+        Assert.assertNotNull(users.get(0).getId());
+        Assert.assertEquals(users.get(0).getId().intValue(), 31);
+        Assert.assertNotNull(users.get(69).getId());
+        Assert.assertEquals(users.get(69).getId().intValue(), 100);
+    }
+
+    @Test
+    public void testLimitWithOffset() {
+
+        QueryParameters q = new QueryParameters();
+        q.setLimit(25);
+        q.setOffset(40);
+
+        List<User> users = JPAUtils.queryEntities(em, User.class, q);
+
+        Assert.assertNotNull(users);
+        Assert.assertEquals(users.size(), 25);
+        Assert.assertNotNull(users.get(0).getId());
+        Assert.assertEquals(users.get(0).getId().intValue(), 41);
+        Assert.assertNotNull(users.get(24).getId());
+        Assert.assertEquals(users.get(24).getId().intValue(), 65);
+    }
+
+    @Test
+    public void testLimitTooBig() {
+
+        QueryParameters q = new QueryParameters();
+        q.setLimit(300);
+
+        List<User> users = JPAUtils.queryEntities(em, User.class, q);
+
+        Assert.assertNotNull(users);
+        Assert.assertEquals(users.size(), 100);
+    }
+
+    @Test
+    public void testOffsetOutOfBounds() {
+
+        QueryParameters q = new QueryParameters();
+        q.setOffset(200);
+
+        List<User> users = JPAUtils.queryEntities(em, User.class, q);
+
+        Assert.assertNotNull(users);
+        Assert.assertEquals(users.size(), 0);
     }
 
     @Test
